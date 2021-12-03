589797096d52e8e24679837d0ddd8e9d2451f9d5
Hongsuk Ryu <koreanryu96@gmail.com>
2021-12-03 13:34:40 +0900

    initial setting

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
