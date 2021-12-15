788ebe919d19f79a026eaad2c2ecdba373dfe2a8
Hongsuk Ryu <koreanryu96@gmail.com>
2021-12-15 15:33:57 +0900

    markdown to js and sidebar created

---
+++ src/components/markdown/Markdown.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+import { Typography, Link, Box } from '@mui/material';
+
+function MarkdownListItem(props: any) {
+  return <Box component="li" sx={{ mt: 1, typography: 'body1' }} {...props} />;
+}
+
+const options = {
+  overrides: {
+    h1: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'h4',
+        component: 'h1',
+      },
+    },
+    h2: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'h6', component: 'h2' },
+    },
+    h3: {
+      component: Typography,
+      props: { gutterBottom: true, variant: 'subtitle1' },
+    },
+    h4: {
+      component: Typography,
+      props: {
+        gutterBottom: true,
+        variant: 'caption',
+        paragraph: true,
+      },
+    },
+    p: {
+      component: Typography,
+      props: { paragraph: true },
+    },
+    a: { component: Link },
+    li: {
+      component: MarkdownListItem,
+    },
+  },
+};
+
+export default function Markdown(props: any) {
+  return <ReactMarkdown options={options} {...props} />;
+}
