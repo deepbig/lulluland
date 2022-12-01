625cb7131b5f12c6ddadcd21ac2c31224378ed97
Hongsuk <hongsuk.ryu@silcraodsoft.com>
2022-12-01 15:49:47 +0900

    [bug-fix] deleted unused components

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   AppBar,
@@ -9,7 +9,6 @@ import {
   Button,
   useTheme,
   useMediaQuery,
-  Fade,
   Box,
   Fab,
   useScrollTrigger,
@@ -22,39 +21,6 @@ import { getUser } from 'modules/user';
 import MenuListItems from './MenuListItems';
 import { drawerWidth } from 'lib';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-const ScrollTop = (props: { children: React.ReactElement }) => {
-  const { children } = props;
-
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 100,
-  });
-
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const anchor = (
-      (event.target as HTMLDivElement).ownerDocument || document
-    ).querySelector('#back-to-top-anchor');
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        block: 'center',
-      });
-    }
-  };
-
-  return (
-    <Fade in={trigger}>
-      <Box
-        onClick={handleClick}
-        role='presentation'
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Fade>
-  );
-};
 
 export default function NavBar(props: { selectedName: string }) {
   const [open, setOpen] = useState(false);
